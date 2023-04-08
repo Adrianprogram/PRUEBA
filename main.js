3b0e7cacd332daa2429e93d80e4e596e292bdfe9
Adrian <adrianaliguillen13@gmail.com>
2023-04-08 10:52:40 -0500

    primer commit

---
+++ main.js
@@ -0,0 +1,8 @@
+let respuesta = prompt("eres de la repa?");
+
+if (respuesta.toLowerCase() === "si") {
+    alert("puedes ver")
+} else {
+    document.getElementById("contenido").style.display = "none"
+
+} 
